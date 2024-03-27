8bf4f463034127eac47638c5f91fad2c93cdd327
Oscariolo <oscariolopinop21@gmail.com>
2024-03-26 22:24:26 -0500

    commit of export_based_implem

--- player.gd
+++ player.gd
@@ -1,41 +1,35 @@
 extends CharacterBody2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var facing_vector = Vector2(0,0)
+@export var move_speed = 200.0
+@export var jump_height : float
+@export var jump_time_to_peak : float
+@export var jump_time_to_descent : float
 
-const MAX_FALL_SPEED = 200
-const MAX_WALK_SPEED = 300
-const acc = 50.0
-
-
-func _ready():
-	print(gravity)
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 func _physics_process(delta):
-	facing_vector = Input.get_vector("move_left","move_right","look_up","look_down")
+	velocity.y += get_gravity() * delta
+	velocity.x = get_input_velocity() * move_speed
 	
-	#apply gravity 
-	if velocity.y <= MAX_FALL_SPEED:
-		velocity.y += gravity * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump()
 	
-	#walking acceleration
-	if facing_vector[0] == 1:
-		velocity.x = min(velocity.x + acc,MAX_WALK_SPEED)
-	if facing_vector[0] == -1:
-		velocity.x = max(velocity.x - acc,-MAX_WALK_SPEED)
-	if facing_vector[0] == 0:
-		velocity.x = lerp(velocity.x,0.0,0.4)	
-	
-	#jump
-
 	move_and_slide()
 
-func _walk(delta):
-	pass
+func get_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+func jump():
+	velocity.y = jump_velocity
+
+func get_input_velocity() -> float:
+	var horizontal := 0.0
 	
-func _jump(delta):
-	pass
+	if Input.is_action_pressed("left"):
+		horizontal -= 1.0
+	if Input.is_action_pressed("right"):
+		horizontal += 1.0
 	
-func _dash(delta):
-	pass
+	return horizontal
