21632a26019b369673a52ffb4e8c78accef6b3e0
Oscariolo <oscariolopinop21@gmail.com>
2024-03-26 23:04:48 -0500

    Revamped jump logic to leverage a mathematical approach for jump height, air time, and fall time.

--- player.gd
+++ player.gd
@@ -4,30 +4,38 @@ extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facing_vector = Vector2(0,0)
 
+#GRAVITY AND HORIZONTAL MOVEMENT
 const MAX_FALL_SPEED = 200
 const MAX_WALK_SPEED = 300
 const acc = 50.0
+#JUMP MECHANIC
+@export var jump_height : float
+@export var jump_time_to_peak : float
+@export var jump_time_to_descent : float
 
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
-func _ready():
-	print(gravity)
 
 func _physics_process(delta):
 	facing_vector = Input.get_vector("move_left","move_right","look_up","look_down")
 	
 	#apply gravity 
-	if velocity.y <= MAX_FALL_SPEED:
-		velocity.y += gravity * delta
+	#if velocity.y <= MAX_FALL_SPEED: #base 1
+		#velocity.y += gravity * delta
+	velocity.y += get_gravity() * delta
 	
-	#walking acceleration
+	#walking
 	if facing_vector[0] == 1:
 		velocity.x = min(velocity.x + acc,MAX_WALK_SPEED)
 	if facing_vector[0] == -1:
 		velocity.x = max(velocity.x - acc,-MAX_WALK_SPEED)
 	if facing_vector[0] == 0:
 		velocity.x = lerp(velocity.x,0.0,0.4)	
-	
 	#jump
+	if Input.is_action_just_pressed("jump"):
+		_jump(delta)
 
 	move_and_slide()
 
@@ -35,7 +43,10 @@ func _walk(delta):
 	pass
 	
 func _jump(delta):
-	pass
+	velocity.y = jump_velocity
 	
 func _dash(delta):
 	pass
+
+func get_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
